0943ee1c952178e9bed19b3fb8a8b40b63c12d16
kerami <keramiozsoy@hotmail.com>
2021-10-28 00:50:56 +0300

    README.md updated

---
+++ java101/src/main/java/examples/ValueAssignToVariable.java
@@ -0,0 +1,18 @@
+package examples;
+
+
+public class ValueAssignToVariable {
+
+    public static int a = 5;
+    public static int b;
+    public static String javaWorld = "JavaWorld";
+
+
+    public static void main(String[] args) {
+        b = 7;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(javaWorld);
+    }
+}
