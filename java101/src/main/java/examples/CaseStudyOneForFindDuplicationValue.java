bdbd6f837d1d5d27dc4a501b4d12712012ee4357
kerami <keramiozsoy@hotmail.com>
2021-11-05 01:44:32 +0300

    casestudy

---
+++ java101/src/main/java/examples/CaseStudyOneForFindDuplicationValue.java
@@ -0,0 +1,22 @@
+package examples;
+
+public class CaseStudyOneForFindDuplicationValue {
+    public static void main(String[] args) {
+        int[] intArray = { 7, 9, 5, 5, 1, 3 };
+
+        for(int i = intArray.length - 1 ; i >= 0; i--){
+            // System.out.println(intArray[i]);
+
+
+            /*
+            if ( intArray[i] == intArray[i-1]){
+                System.out.println(intArray[i]);
+                break;
+            }
+
+            */
+
+        }
+
+    }
+}
