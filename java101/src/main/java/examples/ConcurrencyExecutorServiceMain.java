208b6725be442eca1293580a4e1e3e6182bceb23
kerami <keramiozsoy@hotmail.com>
2021-11-15 21:08:40 +0300

    Readme.md

--- java101/src/main/java/examples/ConcurrencyExecutorServiceMain.java
+++ java101/src/main/java/examples/ConcurrencyExecutorServiceMain.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Executors;
 public class ConcurrencyExecutorServiceMain {
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
+        // above line is instead of start()
 
         for (int i = 1; i <= 5; i++) {
             int count = i;
