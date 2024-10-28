ec48eb7dae0c1255a03af19373b7d6f481f92b38
kerami <keramidev@gmail.com>
2024-10-28 15:01:51 +0300

    Update StringFindingIndexGivenString.java

--- java101/src/main/java/examples/StringFindingIndexGivenString.java
+++ java101/src/main/java/examples/StringFindingIndexGivenString.java
@@ -2,8 +2,8 @@ package examples;
 
 public class StringFindingIndexGivenString {
     public static void main(String[] args) {
-        // indexOf will return first match index of position
-        // it will count white space
+        // indexOf() finds and returns index of first occurrence of specified character in this String
+        // indexOf() cares and counts white spaces
         String txt = "Where is my key !!!! This is my key !!!";
         System.out.println(txt.indexOf("key"));
     }
