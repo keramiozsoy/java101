7106989302d5018ad71419c05369fc027cce9f6b
kerami <keramidev@gmail.com>
2024-10-28 15:10:23 +0300

    Update StringSubString.java

--- java101/src/main/java/examples/StringSubString.java
+++ java101/src/main/java/examples/StringSubString.java
@@ -3,10 +3,10 @@ package examples;
 public class StringSubString {
     public static void main(String[] args) {
         String loremText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
-        // this will give the string from index 0 to 5
+        // Returns the string starting from index 0 to 5
         System.out.println(loremText.substring(0,5));
 
-        // this will give the string from index 5 to the end of the string
+        // Returns the string starting from index 5 to the end of the string
         System.out.println(loremText.substring(5));
     }
 }
